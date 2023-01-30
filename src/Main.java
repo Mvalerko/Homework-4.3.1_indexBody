3011d070b31cfe4b93f7d661fc1e6a5622ad66b1
Mikhail Valerko <mixelv@mail.ru>
2023-01-30 19:40:24 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //Объявляем переменную с типом данных класса BonusMilesService и начначаем в качестве нее класс BonusMilesService
+        BonusMilesService service = new BonusMilesService();
+        // Объявляем переменную, в которую указываем цену билета
+        int price = 16_584;
+        // Объявляем переменную, значением котрой будет результат работы вызванного метода
+        // calculate, который живет в классе BonusMilesService, с указанным в качестве параметра значением переменной price
+        int miles = service.calculate(price);
+        //Получаем возврат в виде результата работы метода calculate и выводим его вместе с текстом для пользователя
+        System.out.println("Благодарим за покупку и рассчитываем Ваш бонус...");
+        System.out.println("Итоговое количство бонусных миль: " + miles + " миль(я)");
+    }
+}
